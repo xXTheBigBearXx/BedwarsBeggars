cb83dc1316db4cc5a97b5e2ae2da91cadf6a5b77
Permanently <me@permanently.xyz>
2021-04-20 14:49:10 +0100

    Improved regex filters
    Fixed false positives, added more begging filters

--- regex.js
+++ regex.js
@@ -1,8 +1,8 @@
 const reg = [
-    /(will|w(o)?uld)(\s{0,}\w{1,}){0,}\s{1,}for\s{0,}a?\s{1,}(m\.?v\.?p|v\.?i\.?p|rank|\+|(.{2,10}\s{0,})?cosmetics?)/gmi, // will/would _____ for (a) m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)
-    /(gi{1,}mm{1,}e{1,}|(f|g)e{1,}t{1,}|give)\s{0,}(me{1,})?\s{0,}a?\s{1,}(m\.?v\.?p|v\.?i\.?p|rank|\+|(.{2,10}\s{0,})?cosmetics?)/gmi, // gimme/(f|g)et/give (me) (a) m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)
+    /(will|wo?uld)(\s{0,}\w{1,}){0,16}\s{1,}for\s{0,}a?\s{1,}(m\.?v\.?p|v\.?i\.?p|rank|\+|(\w{1,}\s{0,})cosmetics?)/gmi, // will/would _____ for (a) m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)
+    /(gi{1,}mm{1,}e{1,}|(f|g)e{1,}t{1,}|give)\s{0,}(me{1,})?\s{0,}a?\s{1,}(\d{0,6}(\s|-){0,}(mon?ths?|years?|days?)?\s{1,})?(m\.?v\.?p|v\.?i\.?p|rank|\+|(.{2,10}\s{0,})?cosmetics?|go{1,}l{1,}d{1,})/gmi, // gimme/(f|g)et/give (me) (a) ([0-9] month(s)/year(s)/day(s)) m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)/gold
     /i?\s{1,}(want|only)\s{1,}(want|only)?\s{1,}(m\.?v\.?p|v\.?i\.?p|rank|\+|(.{2,10}\s{0,})?cosmetics?)/gmi, // (i) "only want"/"want only" m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)
-    /(donat{1,}e{1,}|gif{1,}t{1,}|ra{1,}n{1,}k{1,}\s{1,}m{1,}e{1,}|bu{1,}y{1,})(\s{1,}m{1,}e{1,})?(\s{0,}a?\s{1,}(m\.?v\.?p|v\.?i\.?p|rank|\+|(.{2,10}\s{0,})?cosmetics?))?/gmi, // donate/gift/rank/buy (me) ((a) m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)) [note: excludes "ranked bedwars"]
+    /(donat{1,}e{1,}|gif{1,}t{1,}|ra{1,}n{1,}k{1,}\s{1,}m{1,}e{1,}|bu{1,}y{1,})\s{1,}(m{1,}e{1,})?(\s{0,}a\s{0,})?(m{1,}e{1,}|m\.?v\.?p|v\.?i\.?p|rank|\+|(.{2,10}\s{0,})?cosmetics?)/gmi, // donate/gift/rank/buy (me) ((a) m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)) [note: excludes "ranked bedwars"]
     /any{1,}\s{0,}.{0,10}\s{1,}gifters\s{0,}\?{0,}/gmi, // any (*) gifters( ?)
     /p{1,}l{1,}(e{1,}a{1,}s{1,}e{1,}|s{1,}|z{1,})\s{1,}(m\.?v\.?p|v\.?i\.?p|rank|\+|(.{2,10}\s{0,})?cosmetics?)/gmi, // pls/please m(.)v(.)p/v(.)i(.)p/rank/+/(* )cosmetic(s)
     /(m\.?v\.?p\+?|v\.?i\.?p\+?|rank|\+\+|(.{2,10}\s{0,})?cosmetics?)\s{1,}p{1,}l{1,}(e{1,}a{1,}s{1,}e{1,}|s{1,}|z{1,})/gim, // m(.)v(.)p(+)/v(.)i(.)p(+)/rank/++/(* )cosmetic(s) pls/please
