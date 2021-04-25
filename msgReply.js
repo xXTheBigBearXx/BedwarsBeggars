3fedff8a5bee0f0a812f398ea20651364986f5ce
Alfie <alfiearmitage@gmail.com>
2021-04-25 17:34:20 +0100

    Create msgReply.js

---
+++ msgReply.js
@@ -0,0 +1,14 @@
+const msgReply = [
+  'Hey! Loser.',
+  'xXTheLargeBearXx',
+  'Armay likes egirls.',
+  'Dude, stop messaging me.',
+  'I will literally block you. Go away.',
+  'Permanently is a catboy.',
+  'Permanently? More like Permanentgay.',
+  'What&#39;s that? You like men? Woah, that&#39;s pretty cool.'
+  '@Permanently#0001, @Jayًًً#7412, @alfie#1111'
+  'Oh my god, is that Joe?'
+]
+
+module.exports = msgReply;
