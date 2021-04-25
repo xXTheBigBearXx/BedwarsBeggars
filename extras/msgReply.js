634bbabf2fbfd5fbcf0ca86feca243310cde6b37
Permanently <me@permanently.xyz>
2021-04-25 18:26:58 +0100

    Switched conditional to regex
    because i'm paranoid that some other similar message that starts with "From " will trigger the bot, even though the chances of that are extremely low

--- extras/msgReply.js
+++ extras/msgReply.js
@@ -11,7 +11,7 @@ const msgReply = [
 ]
 
 module.exports = function (mc, msg) {
-  if (msg.startsWith("From ")) {
+  if (/From\s(\[.{1,}\]\s)?\w{1,16}\:\s.{1,}/mi.test(msg)) {
     let rand = Math.floor(Math.random() * (msgReply.length - 1));
     mc.chat("/r " + msgReply[rand]);
     console.log("I sent response no. " + rand + " to someone!");
