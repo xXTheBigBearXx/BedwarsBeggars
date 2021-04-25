7417629090290bde1da73bea131a988cca136701
Permanently <me@permanently.xyz>
2021-04-25 17:40:51 +0100

    Fixed formatting

--- msgReply.js
+++ msgReply.js
@@ -6,8 +6,8 @@ const msgReply = [
   'I will literally block you. Go away.',
   'Permanently is a catboy.',
   'Permanently? More like Permanentgay.',
-  'What&#39;s that? You like men? Woah, that&#39;s pretty cool.'
-  '@Permanently#0001, @Jayًًً#7412, @alfie#1111'
+  'What\'s that? You like men? Woah, that\'s pretty cool.',
+  '@Permanently#0001, @Jayًًً#7412, @alfie#1111',
   'Oh my god, is that Joe?'
 ]
 
